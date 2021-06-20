16154956f604c3e003659105821ad3fa9b68bdd4
Tausif Shakeel <tausif.shakeel@vipps.no>
2021-06-20 16:49:53 +0200

    Added swagger

--- src/main/java/com/games/kalah/swagger/SwaggerConfig.java
+++ src/main/java/com/games/kalah/swagger/SwaggerConfig.java
@@ -4,36 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    private ApiInfo apiInfo = new ApiInfo(
-            "Kalah for Backbase interview",
-            "A simple 6-stone Kalah game",
-            "1.0.0",
-            "",
-            new Contact("Tausif Shakeel", "https://www.linkedin.com/in/sk-tausif-shakeel-28656a30/", "tausif.056@gmail.com"),
-            "",
-            "",
-            Collections.emptyList()
-    );
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.games.kalah.ui.rest"))
+                .apis(RequestHandlerSelectors.basePackage("com.games.kalah.controller"))
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo);
+                .build();
     }
 }
